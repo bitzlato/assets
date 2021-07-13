193d2c4336fc28b443d1a02c4a8b8573c057f545
Danil Pismenny <danil@brandymint.ru>
2021-07-13 15:11:04 +0300

    Update deploy to use default branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,4 +17,13 @@ set :keep_releases, 10
 set :roles, %w[web].freeze
 set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:application)}" }
 
-set :branch, ENV.fetch('BRANCH', 'main')
+default_branch = 'master'
+current_branch = `git rev-parse --abbrev-ref HEAD`.chomp
+
+if ENV.key? 'BRANCH'
+  set :branch, ENV.fetch('BRANCH')
+elsif default_branch == current_branch
+  set :branch, default_branch
+else
+  ask(:branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp })
+end
